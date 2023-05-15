fee0ea1d0b5afba5436813ddc4774c2751559e42
Alicia <aliciavm@hotmail.fr>
2023-05-15 14:48:44 +0200

    pull

--- App.js
+++ App.js
@@ -1,23 +1,21 @@
-import { StyleSheet} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import  HomeScreen  from './screens/HomeScreen';
-import  PreferenceScreen  from './screens/PreferenceScreen';
-import LoginScreen  from './screens/LoginScreen';
-import  SignUpScreen  from './screens/SignUpScreen';
-import SignUpSignInScreen from './screens/SignUpSignInScreen';
-import { useFonts } from 'expo-font';
-
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import PreferenceScreen from "./screens/PreferenceScreen";
+import LoginScreen from "./screens/LoginScreen";
+import SignUpScreen from "./screens/SignUpScreen";
+import SignUpSignInScreen from "./screens/SignUpSignInScreen";
+import { useFonts } from "expo-font";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-
   const [loaded] = useFonts({
-    PoppinsBlack: require('./assets/fonts/Poppins-Black.ttf'),
-    PoppinsBlackItalic: require('./assets/fonts/Poppins-BlackItalic.ttf'),
-    PoppinsBold: require('./assets/fonts/Poppins-Bold.ttf'),
-    PoppinsRegular: require('./assets/fonts/Poppins-Regular.ttf'),
+    PoppinsBlack: require("./assets/fonts/Poppins-Black.ttf"),
+    PoppinsBlackItalic: require("./assets/fonts/Poppins-BlackItalic.ttf"),
+    PoppinsBold: require("./assets/fonts/Poppins-Bold"),
+    PoppinsRegular: require("./assets/fonts/Poppins-Regular"),
   });
 
   if (!loaded) {
@@ -25,22 +23,25 @@ export default function App() {
   }
   return (
     <NavigationContainer>
-    <Stack.Navigator screenOptions={{ headerShown: false }} style={styles.container}>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Preference" component={PreferenceScreen} />
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="SignUp" component={SignUpScreen} />
-      <Stack.Screen name="SignUpSignIn" component={SignUpSignInScreen} />
-    </Stack.Navigator>
-  </NavigationContainer>
-  )
+      <Stack.Navigator
+        screenOptions={{ headerShown: false }}
+        style={styles.container}
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Preference" component={PreferenceScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="SignUpSignIn" component={SignUpSignInScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- screens/PreferenceScreen.js
+++ screens/PreferenceScreen.js
@@ -1,15 +1,179 @@
-import { View, Text, Button } from "react-native"
+import { View, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
 
-export default function PreferenceScreen({ navigation }){
+export default function PreferenceScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Choisir vos 3 styles favoris :</Text>
+      <View style={styles.stylesContainer}>
+        <TouchableOpacity style={styles.jungleBtn}>
+          <Text style={styles.jungleTxt}>Jungle/Drum'n Bass</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.breakbeatBtn}>
+          <Text style={styles.breakbeatTxt}>Breakbeat</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.bassBtn}>
+          <Text style={styles.bassTxt}>Bass Music</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.tranceBtn}>
+          <Text style={styles.tranceTxt}>Trance</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.technoBtn}>
+          <Text style={styles.technoTxt}>Techno</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.houseBtn}>
+          <Text style={styles.houseTxt}>House</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.experimentalBtn}>
+          <Text style={styles.experimentalTxt}>Experimental</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.validerBtn}>
+          <Text style={styles.validerTxt}>Valider</Text>
+        </TouchableOpacity>
+      </View>
 
-    return(
-        <View>
-            <Text>Preference Screen</Text>
-
-            <Button
-       title="Go to Home"
-       onPress={() => navigation.navigate('Home')}
-     />
-        </View>
-    )
+      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#262626",
+    alignItems: "center",
+    justifyContent: "flex-end",
+  },
+  title: {
+    flex: 0.8,
+    alignItems: "center",
+    fontFamily: "PoppinsBold",
+    color: "white",
+  },
+  stylesContainer: {
+    flex: 2,
+    flexDirection: "column",
+    justifyContent: "space-around",
+    alignContent: "center",
+  },
+  jungleBtn: {
+    width: 300,
+    height: 40,
+    alignItems: "center",
+    marginTop: 20,
+    paddingTop: 8,
+    backgroundColor: "#262626",
+    borderRadius: 10,
+    borderColor: "white",
+    borderWidth: 1,
+  },
+  jungleTxt: {
+    color: "white",
+    fontFamily: "PoppinsRegular",
+  },
+  breakbeatBtn: {
+    width: 300,
+    height: 40,
+    alignItems: "center",
+    marginTop: 20,
+    paddingTop: 8,
+    backgroundColor: "#262626",
+    borderRadius: 10,
+    borderColor: "white",
+    borderWidth: 1,
+  },
+  breakbeatTxt: {
+    color: "white",
+    fontFamily: "PoppinsRegular",
+  },
+  bassBtn: {
+    width: 300,
+    height: 40,
+    alignItems: "center",
+    marginTop: 20,
+    paddingTop: 8,
+    backgroundColor: "#262626",
+    borderRadius: 10,
+    borderColor: "white",
+    borderWidth: 1,
+  },
+  bassTxt: {
+    color: "white",
+    fontFamily: "PoppinsRegular",
+  },
+  tranceBtn: {
+    width: 300,
+    height: 40,
+    alignItems: "center",
+    marginTop: 20,
+    paddingTop: 8,
+    backgroundColor: "#262626",
+    borderRadius: 10,
+    borderColor: "white",
+    borderWidth: 1,
+  },
+  tranceTxt: {
+    color: "white",
+    fontFamily: "PoppinsRegular",
+  },
+  technoBtn: {
+    width: 300,
+    height: 40,
+    alignItems: "center",
+    marginTop: 20,
+    paddingTop: 8,
+    backgroundColor: "#262626",
+    borderRadius: 10,
+    borderColor: "white",
+    borderWidth: 1,
+  },
+  technoTxt: {
+    color: "white",
+    fontFamily: "PoppinsRegular",
+  },
+  houseBtn: {
+    width: 300,
+    height: 40,
+    alignItems: "center",
+    marginTop: 20,
+    paddingTop: 8,
+    backgroundColor: "#262626",
+    borderRadius: 10,
+    borderColor: "white",
+    borderWidth: 1,
+  },
+  houseTxt: {
+    color: "white",
+    fontFamily: "PoppinsRegular",
+  },
+  experimentalBtn: {
+    width: 300,
+    height: 40,
+    alignItems: "center",
+    marginTop: 20,
+    paddingTop: 8,
+    backgroundColor: "#262626",
+    borderRadius: 10,
+    borderColor: "white",
+    borderWidth: 1,
+  },
+  experimentalTxt: {
+    color: "white",
+    fontFamily: "PoppinsRegular",
+  },
+  validerBtn: {
+    flex: 0.4,
+    width: 150,
+    alignItems: "center",
+    justifyContent: "flex-end",
+    marginTop: 50,
+    paddingTop: 8,
+    backgroundColor: "#7C4DFF",
+    borderRadius: 10,
+    marginLeft: 70,
+    paddingBottom: 10,
+  },
+  validerTxt: {
+    color: "white",
+    fontFamily: "PoppinsBold",
+  },
+});
