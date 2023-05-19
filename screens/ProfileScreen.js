0dcf96c05777c60e5aa5dbe72ba4fd406749f207
Camy <camyms13@gmail.com>
2023-05-19 14:28:13 +0200

    profile

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -9,13 +9,43 @@ import {
 } from "react-native";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import NavbarScreen from "./NavbarScreen";
+import { useEffect, useState } from "react";
+import { PORT } from "@env";
 
 export default function ProfileScreen() {
+  const [userData, setUserData] = useState([]);
+
+  const user = useSelector((state) => state.user.value);
+
+  useEffect(() => {
+    fetch(`http://${PORT}:3000/users/userdata/${user.token}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setUserData(data);
+      });
+  }, []);
+
+  const UserData = userData.map((data, i) => {
+    return (
+      <View
+        key={i}
+        pseudo={data.photo}
+        email={data.name}
+        password={data.password}
+        avatar={data.avatar}
+        ville={data.ville}
+        tags={[data.tags]}
+        tickets={[data.tickets]}
+        like={[data.likes]}
+      />
+    );
+  });
+
   return (
     <View style={styles.main}>
       <NavbarScreen></NavbarScreen>
       <TouchableOpacity>
-        <View style={styles.content}></View>
+        <View style={styles.content}>{UserData}</View>
       </TouchableOpacity>
     </View>
   );
