523a022c9fdbc01cf22f77228faed964ba572ab8
Camy <camyms13@gmail.com>
2023-05-19 11:46:43 +0200

    profile

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,27 +1,32 @@
-import React from 'react';
-import { Text, SafeAreaView, TouchableOpacity, StyleSheet, Image, View } from 'react-native';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import NavbarScreen from './NavbarScreen';
+import React from "react";
+import {
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  View,
+} from "react-native";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import NavbarScreen from "./NavbarScreen";
 
-export default function ProfileScreen(){
-
-
-    return(
-        <View style={styles.main}>
-            <NavbarScreen></NavbarScreen>
-            <View style={styles.content}></View>
-        </View>
-
-    )
+export default function ProfileScreen() {
+  return (
+    <View style={styles.main}>
+      <NavbarScreen></NavbarScreen>
+      <TouchableOpacity>
+        <View style={styles.content}></View>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-
-    main: {
-        flex: 1,
-        backgroundColor: 'black'
-    },
-    content: {
-        backgroundColor: "#262626"
-    }
-})
+  main: {
+    flex: 1,
+    backgroundColor: "black",
+  },
+  content: {
+    backgroundColor: "#262626",
+  },
+});
