ad42232619d05d7ba1448e88ead07bd5f7af7e94
Camy <camyms13@gmail.com>
2023-05-22 17:25:46 +0200

    branchswitch

--- reducers/user.js
+++ reducers/user.js
@@ -1,18 +1,24 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  value: { token: null, pseudo: null, email: null, tags: [], likedEvents: [], },
+  value: {
+    token: null,
+    pseudo: null,
+    email: null,
+    tags: [],
+    likedEvents: [],
+    ville: null,
+  },
 };
 
 export const userSlice = createSlice({
-  name: 'user',
+  name: "user",
   initialState,
   reducers: {
     login: (state, action) => {
       state.value.token = action.payload.token;
       state.value.pseudo = action.payload.pseudo;
       state.value.email = action.payload.email;
-
     },
     logout: (state) => {
       state.value.token = null;
@@ -20,13 +26,14 @@ export const userSlice = createSlice({
       state.value.email = null;
     },
     addTags: (state, action) => {
-      console.log("actionPayload", action.payload)
+      console.log("actionPayload", action.payload);
       state.value.tags = action.payload;
-      
     },
-   
+    ville: (state, action) => {
+      state.value.ville = action.payload.ville;
+    },
   },
 });
 
-export const { login, logout, addTags } = userSlice.actions;
+export const { login, logout, addTags, ville } = userSlice.actions;
 export default userSlice.reducer;
--- screens/NavbarScreen.js
+++ screens/NavbarScreen.js
@@ -1,42 +1,86 @@
-import React from 'react';
-import { Text, SafeAreaView, TouchableOpacity, StyleSheet, Image, View } from 'react-native';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import React from "react";
+import {
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  View,
+} from "react-native";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 import { useNavigation } from "@react-navigation/native";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { ville } from "../reducers/user";
 
 export default function NavbarScreen() {
+  const user = useSelector((state) => state.user.value);
+
+  const [userCity, setUserCity] = useState([]);
 
   const navigation = useNavigation();
-  
+
+  useEffect(() => {
+    fetch(`http://${PORT}:3000/users/userdata/${user.token}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data, "c la data");
+        setUserCity(data.user);
+      });
+  }, []);
+
+  const handleSave = () => {
+    fetch(`http://${PORT}:3000/users/cityUpdate`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        token: user.token, // Assuming you have the user token available
+        ville: userCity,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("console log retour fetch", data);
+        if (data.modifiedCount > 0) {
+          dispatch(ville({ ville: ville }));
+        }
+      });
+  };
   return (
-   
-        <SafeAreaView style={styles.navbar}>
-        <Text style={styles.ville}>Marseille</Text>
-        <View style={styles.centerContainer}>
-            <Image source={require('../assets/logo1.png')} style={styles.logo} />
-        </View>
-        <TouchableOpacity style={styles.heart} onPress={() => navigation.navigate('Like')}>
-            <FontAwesome name='heart-o' size={30} color='white'/>
-        </TouchableOpacity>
-        </SafeAreaView>
+    <SafeAreaView style={styles.navbar}>
+      <TouchableOpacity>
+        <Text style={styles.ville}>{userCity.ville}</Text>
+      </TouchableOpacity>
+      <View style={styles.centerContainer}>
+        <Image source={require("../assets/logo1.png")} style={styles.logo} />
+      </View>
+      <TouchableOpacity
+        style={styles.heart}
+        onPress={() => navigation.navigate("Like")}
+      >
+        <FontAwesome name="heart-o" size={30} color="white" />
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  
   navbar: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'evenly',
-    backgroundColor: 'black',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "evenly",
+    backgroundColor: "black",
     height: 120,
   },
   centerContainer: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
   },
   ville: {
-    color: 'white',
-    fontFamily: 'PoppinsRegular',
+    color: "white",
+    fontFamily: "PoppinsRegular",
     fontSize: 16,
     marginLeft: 10,
   },
